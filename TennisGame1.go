bef9f8764f0e4fa48a5d3575aeb24ae814bb940f
Philippe Alfaiate <allission@gmail.com>
2020-05-31 15:05:19 +0200

    [FINAL] add GetFormatedScore and const

--- TennisGame1.go
+++ TennisGame1.go
@@ -1,6 +1,8 @@
 package tenniskata
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type tennisGame1 struct {
 	m_score1    int
@@ -26,30 +28,43 @@ func (game *tennisGame1) WonPoint(playerName string) {
 	}
 }
 
+const (
+	ADVANTAGE string = "Advantage"
+	WIN              = "Win for"
+	ALL              = "All"
+)
+
+func GetFormatedScore(fisrt string, second string, separator ...string) string {
+	def := " "
+	if separator != nil {
+		def = separator[0]
+	}
+	return fmt.Sprint(fisrt, def, second)
+}
+
 func (game *tennisGame1) GetScore() string {
 
-	array := [4]string{"Love", "Fifteen", "Thirty", "Forty"}
+	score := [4]string{"Love", "Fifteen", "Thirty", "Forty"}
 
 	if game.m_score1 == game.m_score2 {
 		if game.m_score1 > 2 {
 			return "Deuce"
 		}
-		return fmt.Sprintf("%s-All", array[game.m_score1])
-	} else if game.m_score1 >= 4 || game.m_score2 >= 4 {
+		return GetFormatedScore(score[game.m_score1], ALL, "-")
+	}
+	if game.m_score1 >= 4 || game.m_score2 >= 4 {
 		minusResult := game.m_score1 - game.m_score2
 		switch {
 		case minusResult == 1:
-			return fmt.Sprint("Advantage ", game.player1Name)
+			return GetFormatedScore(ADVANTAGE, game.player1Name)
 		case minusResult == -1:
-			return fmt.Sprint("Advantage ", game.player2Name)
+			return GetFormatedScore(ADVANTAGE, game.player2Name)
 		case minusResult >= 2:
-			return fmt.Sprint("Win for ", game.player1Name)
+			return GetFormatedScore(WIN, game.player1Name)
 		default:
-			return fmt.Sprint("Win for ", game.player2Name)
+			return GetFormatedScore(WIN, game.player2Name)
 		}
-
 	}
 
-	return fmt.Sprintf("%s-%s", array[game.m_score1], array[game.m_score2])
-
+	return GetFormatedScore(score[game.m_score1], score[game.m_score2], "-")
 }
